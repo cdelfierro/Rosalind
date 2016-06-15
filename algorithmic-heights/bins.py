a5bfd0b93a85d493e362b2afd207b54688d94b06
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2016-06-15 14:23:25 -0400

    Optimize bins
    Removes the recursive calls of bins. Now, only uses the index and the
    lower and upper limits to make the binary search inside the array of
    numbers.

--- algorithmic-heights/bins.py
+++ algorithmic-heights/bins.py
@@ -9,17 +9,18 @@ from time import clock
 """
 Binary search
 """
-def bins(A, x, upper, lower=0):
-    if upper == lower:
-        return -1
-    while upper > lower:
-        i = (upper - lower) // 2 + lower
-        if x > A[i]:
-            return bins(A, x, upper, i + 1)
-        elif x < A[i]:
-            return bins(A, x, i, lower)
+def bins(A, search):
+    upper = len(A) - 1
+    lower = 0
+    while upper >= lower:
+        i = (upper + lower) // 2
+        if search > A[i]:
+            lower = i + 1
+        elif search < A[i]:
+            upper = i - 1
         else:
             return i + 1
+    return -1
 
 try:
     with open(sys.argv[1], "r", encoding="utf-8") as ini_file:
@@ -36,7 +37,7 @@ A = [int(i) for i in data[2].split()]
 search = [int(i) for i in data[3].split()]
 
 start = clock()
-finds = [bins(A, i, n) for i in search]
+finds = [bins(A, i) for i in search]
 end = clock()
 time = end - start
 finds = " ".join(str(i) for i in finds)
