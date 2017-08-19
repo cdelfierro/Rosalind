9c3b75b42ea187b88266a463c89fc33e87bbdcb4
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2017-08-19 02:09:38 -0300

    Refactor the code of Fibonacci file

--- algorithmic-heights/fibo.py
+++ algorithmic-heights/fibo.py
@@ -3,47 +3,43 @@
 """
 @author: Carlos Gómez del Fierro
 """
+import argparse
 import sys
-from time import clock
 
-"""
-Recursive fibonacci
-"""
+
 def fib1(n):
+    """ Recursive Fibonacci """
     if n == 0:
         return 0
     if n == 1:
         return 1
     return fib1(n - 1) + fib1(n - 2)
 
-"""
-Faster fibonacci
-"""
+
 def fib2(n):
+    """ Faster Fibonacci """
     a, b = 0, 1
     for i in range(n):
         a, b = b, a + b
     return a
 
-try:
-    x = int(sys.argv[1])
-except ValueError:
-    try:
-        with open(sys.argv[1], "r", encoding="utf-8") as ini_file:
-            x = int(ini_file.read().strip())
-    except FileNotFoundError:
-        print("File not found")
-        sys.exit(1)
-except:
-    x = int(sys.stdin.read())
 
-start = clock()
-fibonacci = fib2(x)
-end = clock()
-time = end - start
+parser = argparse.ArgumentParser(description="Calculate the n-th value of the \
+                                Fibonacci sequence")
+parser.add_argument("infile", nargs="?", default=sys.stdin,
+                    type=argparse.FileType("r", encoding="utf-8"),
+                    help="the file with the input data")
+parser.add_argument("-v", "--verbose", action="store_true")
+args = parser.parse_args()
 
-with open("output/fibo.txt", "w", encoding="utf-8", newline="\n") as f:
-    f.write(str(fibonacci) + "\n")
+n = int(args.infile.read())
+args.infile.close()
 
+fibonacci = fib2(n)
+
+with open("output/fibo.txt", "w", encoding="utf-8", newline="\n") as outfile:
+    outfile.write(str(fibonacci) + "\n")
+
+if args.verbose:
+    print("The value of F({}) is ".format(n), end="")
 print(fibonacci)
-print("Time: {} segs.".format(time))
