7aad9685c85823af603e1e14fdb00e647baa98cf
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2018-05-08 02:37:55 -0300

    Refactoriza fibo

--- algorithmic-heights/fibo.py
+++ algorithmic-heights/fibo.py
@@ -4,26 +4,47 @@
 @author: Carlos Gómez del Fierro
 """
 import argparse
-import sys
 from sequences import linear_fib as F
 
 
-parser = argparse.ArgumentParser(description="Calculate the n-th value of the \
-                                Fibonacci sequence")
-parser.add_argument("infile", nargs="?", default=sys.stdin,
-                    type=argparse.FileType("r", encoding="utf-8"),
-                    help="the file with the input data")
-parser.add_argument("-v", "--verbose", action="store_true")
-args = parser.parse_args()
+def main():
+    """Calculate the n-th value of the Fibonacci sequence"""
+    parser = argparse.ArgumentParser(
+        prog='fibo',
+        description='Calculate the n-th value of the Fibonacci sequence')
+    group = parser.add_mutually_exclusive_group(required=True)
 
-n = int(args.infile.read())
-args.infile.close()
+    parser.add_argument('-v', '--verbose', action='store_true')
+    group.add_argument(
+        '-i', '--infile', type=argparse.FileType('r', encoding='utf-8'),
+        help='the file with the input data')
+    group.add_argument('-n', type=int, help='the n-th value of the Fibonacci sequence to calculate')
+    parser.add_argument(
+        '-o', '--outfile', type=argparse.FileType('w', encoding='utf-8'),
+        help='the file with the output data')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
-fibonacci = F(n)
+    args = parser.parse_args()
 
-with open("output/fibo.txt", "w", encoding="utf-8", newline="\n") as outfile:
-    outfile.write(str(fibonacci) + "\n")
+    if args.n:
+        n = args.n
+    elif args.infile:
+        n = int(args.infile.read())
+        args.infile.close()
 
-if args.verbose:
-    print("The value of F({}) is ".format(n), end="")
-print(fibonacci)
+    fibonacci = F(n)
+
+    if args.outfile:
+        args.outfile.write(str(fibonacci) + '\n')
+        args.outfile.close()
+    else:
+        with open('output/fibo.txt', 'w', encoding='utf-8', newline='\n') as outfile:
+            outfile.write(str(fibonacci) + "\n")
+
+    if args.verbose:
+        print('The value of F({}) is '.format(n), end='')
+    print(fibonacci)
+
+
+if __name__ == '__main__':
+    main()
