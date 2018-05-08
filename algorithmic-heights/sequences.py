9b5b2439ca2c8e9498e31d03221f66bb9134a3d0
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2018-05-08 01:58:40 -0300

    Corrige errores en sequences

--- algorithmic-heights/sequences.py
+++ algorithmic-heights/sequences.py
@@ -11,12 +11,12 @@ def recursive_fib(n):
         return 0
     if n == 1:
         return 1
-    return fib1(n - 1) + fib1(n - 2)
+    return recursive_fib(n - 1) + recursive_fib(n - 2)
 
 
 def linear_fib(n):
     """ Linear Fibonacci """
     a, b = 0, 1
-    for i in range(n):
+    for __ in range(n):
         a, b = b, a + b
     return a
